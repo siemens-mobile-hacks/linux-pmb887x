d1a463f000e1997cea4f1a52d15cdd872f918a82
Alula <git@alula.me>
2026-08-28 03:45:44 +0200

    pinctrl: pmb887x: add boot time pinmux programming via devicetree

---
+++ arch/arm/boot/dts/infineon/ke970-pins.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * LG KE970 board GPIO aliases.
+ */
+#ifndef _KE970_PINS_H
+#define _KE970_PINS_H
+
+#include <dt-bindings/pinctrl/pmb887x.h>
+
+#define KE970_PIN_USB_CHG_EN		39
+#define KE970_PIN_LCD_BACKLIGHT		52
+#define KE970_PIN_SLIDE_KEY_BACKLIGHT		103
+#define KE970_PIN_SCROLL_KEY_A		15
+#define KE970_PIN_SCROLL_KEY_B		62
+#define KE970_PIN_BT_RESET		61
+#define KE970_PIN_SIM_EN		63
+#define KE970_PIN_RPWRON		110
+#define KE970_PIN_SPK_RCV_SEL		111
+#define KE970_PIN_LCD_IF_MODE		96
+#define KE970_PIN_HOOK_DETECT		98
+#define KE970_PIN_HSO_SEL		18
+#define KE970_PIN_AUDIO_AMP_EN		19
+#define KE970_PIN_AF_PWR_EN		55
+#define KE970_PIN_JACK_DETECT		51
+#define KE970_PIN_SLIDE_OPEN		58
+#define KE970_PIN_SCROLL_EN		109
+
+#endif
